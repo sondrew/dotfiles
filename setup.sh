985eebf215efadf6ef0a9f6a9cfefd3b2068cc2b
Sondre Widmark <sondre.widmark@gmail.com>
2018-05-24 21:10:42 +0200

    Automate setup and installation

---
+++ setup.sh
@@ -0,0 +1,21 @@
+# install homebrew
+/usr/bin/ruby -e "$(curl -fsSl https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# make sure everything is ready
+brew update
+brew doctor
+# install Brewfile
+brew bundle
+# useful npm packages
+npm install -g iterm2-tab-set
+npm install -g n
+# copy fish functions from repo into fish function folder
+cp -a fish/functions/. ~/.config/fish/functions
+# copy bash_profile and gitconfig to user folder
+cp {.bash_profile,.gitconfig} ~/.
+# check versions
+python --version
+git --version
+# Change computer name
+# sudo scutil --set HostName <new host name>
+# sudo scutil --set LocalHostName <new host name>
+# sudo scutil --set ComputerName <new name>

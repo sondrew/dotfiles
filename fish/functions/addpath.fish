8f35c0f8c47952e0834043bb6bf5211594615151
Sondre Widmark <sondre.widmark@gmail.com>
2021-10-20 16:42:39 +0200

    Add functions to add and remove in fish user path

---
+++ fish/functions/addpath.fish
@@ -0,0 +1,6 @@
+function addpath
+    echo -e "Modifying PATH: $PATH \n"
+    contains -- $argv $fish_user_paths
+       or set -U fish_user_paths $fish_user_paths $argv
+    echo "Updated PATH: $PATH"
+end
